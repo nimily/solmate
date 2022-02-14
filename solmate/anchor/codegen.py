26c58b43debc934f4ec7dd99d250ebc988f8450c
Joe Howarth <josephehowarth@gmail.com>
2022-02-14 11:31:03 -0600

    Add cli arguments to point to idl and output directories (#1)
    * Add cli arguments to point to idl and output directories
    
    * cleanup
    
    * cleanup cli.py

--- solmate/anchor/codegen.py
+++ solmate/anchor/codegen.py
@@ -1,6 +1,7 @@
 import os
+import re
 
-from typing import Dict, Iterable, Set, Union, Callable
+from typing import List, Dict, Iterable, Set, Union, Callable
 
 from solana.publickey import PublicKey
 from solana.system_program import SYS_PROGRAM_ID
@@ -432,15 +433,15 @@ def program_error_type(editor: CodeEditor):
     return "ProgramError"
 
 
-def cli():
-    for protocol in ("dex", "instruments", "noop_risk_engine"):
-        idl = Idl.from_json_file(f"/Users/nimily/tmp/idl/{protocol}.json")
-        root = "/Users/nimily/Workspaces/python/solmate"
+def cli(idl_dir: str, out_dir: str):
+    for protocol in get_protocols(idl_dir):
+        print(f"Generating code for {protocol}")
+        idl = Idl.from_json_file(f"{idl_dir}/{protocol}.json")
         codegen = CodeGen(
             idl,
             "23423423423434",
             f"dexterity.{protocol}",
-            f"{root}",
+            out_dir,
             external_types={
                 "usize": usize_type,
                 "UnixTimestamp": unix_timestamp_type,
@@ -452,3 +453,16 @@ def cli():
         )
         codegen.generate_code()
         codegen.save_modules()
+
+
+def get_protocols(idl_dir: str) -> List[str]:
+    protocols = []
+    print("Found protocols:")
+    for filename in os.listdir(idl_dir):
+        match = re.search(r"([a-z_]+).json", filename)
+        if match is None:
+            continue
+        protocol = match.groups()[0]
+        print(f"- {protocol}")
+        protocols.append(protocol)
+    return protocols
--- solmate/cli.py
+++ solmate/cli.py
@@ -1,5 +1,22 @@
+import argparse
+import os
+
 from solmate.anchor import codegen
 
 
 def main():
-    codegen.cli()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--idl-dir', type=str, required=False)
+    parser.add_argument('--out-dir', type=str, required=False)
+    args = parser.parse_args()
+
+    if args.out_dir is None:
+        args.out_dir = os.getcwd()
+    if args.idl_dir is None:
+        args.idl_dir = os.getcwd()
+
+    codegen.cli(args.idl_dir, args.out_dir)
+
+
+if __name__ == '__main__':
+    main()
