92500f95a3a9cb28191f3519d19f3c736dc250b6
Joe Howarth <josephehowarth@gmail.com>
2022-02-18 15:29:29 -0500

    cleanup

--- tests/anchor/test_integration.py
+++ tests/anchor/test_integration.py
@@ -1,10 +1,8 @@
-import json
 from pathlib import Path
 
 from solana.keypair import Keypair
 
 from solmate.anchor import codegen
-from solmate.anchor.idl import IdlField
 
 
 def get_project_root() -> Path:
@@ -12,32 +10,19 @@ def get_project_root() -> Path:
 
 
 def test():
+    keypair = Keypair.generate()
     root = str(get_project_root())
     codegen.cli(f"{root}/tests/anchor", root, "codegen", set())
 
-    keypair = Keypair.generate()
-
     from codegen.idl.types import CallBackInfo
+    from codegen.other.types.cross_idl_reference_type import CrossIdlReferenceType
+
     info = CallBackInfo(keypair.public_key, 129)
     _bytes = CallBackInfo.to_bytes(info)
     round_tripped = CallBackInfo.from_bytes(_bytes)
     assert info == round_tripped
 
-    from codegen.other.types.cross_idl_reference_type import CrossIdlReferenceType
     reference_type = CrossIdlReferenceType(keypair.public_key, info)
     _bytes = CrossIdlReferenceType.to_bytes(reference_type)
     round_tripped = CrossIdlReferenceType.from_bytes(_bytes)
     assert reference_type == round_tripped
-
-
-def test2():
-    raw = json.loads(""" 
-    {
-        "name": "callBackInfoField",
-        "type": {
-            "defined": "CallBackInfo"
-        }
-    }
-    """)
-    field = IdlField.from_dict(raw)
-
