d86d2946ae3210e815a30b7ce5ecaad217942d0d
Joe Howarth <josephehowarth@gmail.com>
2022-02-18 14:29:54 -0600

    Cross idl name resolution, add tests (#2)
    * cross idl name resolution, add tests
    
    * cleanup

--- tests/anchor/test_integration.py
+++ tests/anchor/test_integration.py
@@ -1,6 +1,5 @@
-import os
 from pathlib import Path
-from solana.publickey import PublicKey
+
 from solana.keypair import Keypair
 
 from solmate.anchor import codegen
@@ -11,13 +10,19 @@ def get_project_root() -> Path:
 
 
 def test():
+    keypair = Keypair.generate()
     root = str(get_project_root())
-    print(root)
     codegen.cli(f"{root}/tests/anchor", root, "codegen", set())
 
     from codegen.idl.types import CallBackInfo
-    keypair = Keypair.generate()
+    from codegen.other.types.cross_idl_reference_type import CrossIdlReferenceType
+
     info = CallBackInfo(keypair.public_key, 129)
     _bytes = CallBackInfo.to_bytes(info)
     round_tripped = CallBackInfo.from_bytes(_bytes)
     assert info == round_tripped
+
+    reference_type = CrossIdlReferenceType(keypair.public_key, info)
+    _bytes = CrossIdlReferenceType.to_bytes(reference_type)
+    round_tripped = CrossIdlReferenceType.from_bytes(_bytes)
+    assert reference_type == round_tripped
