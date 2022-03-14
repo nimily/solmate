1bc02c70ec6beb17ec07f75c59e5db8fdb23794d
Joe Howarth <josephehowarth@gmail.com>
2022-03-14 13:09:01 -0400

    typo

--- solmate/pids.py
+++ solmate/pids.py
@@ -1,11 +1,12 @@
 from solana.publickey import PublicKey
+from typing import Dict
 
-_PIDS = dict()
+_PIDS: Dict[str, PublicKey] = dict()
 
 
-def get_pids_or_default(protocol_name: str, default: PublicKey):
+def get_pid_or_default(protocol_name: str, default: PublicKey):
     global _PIDS
-    return _PIDS.get(key=protocol_name, default=default)
+    return _PIDS.get(protocol_name, default)
 
 
 def set_pid_by_protocol_name(protocol_name: str, pid: PublicKey):
