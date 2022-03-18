91d1e170793885845be4681500323c8e82b63b45
Joe Howarth <josephehowarth@gmail.com>
2022-03-18 15:13:46 -0400

    help messages + readme updates

--- solmate/cli.py
+++ solmate/cli.py
@@ -29,25 +29,36 @@ def default_account_gen(name_, value_):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--idl", type=str, required=True)
-    parser.add_argument("--addrs", nargs="+", type=str, required=True, default=[])
-    parser.add_argument("--root-dir", type=str, required=True, default=os.getcwd())
-    parser.add_argument("--module", type=str, required=True)
-    parser.add_argument("--skip-types", nargs="+", required=False, default=[])
+    parser.add_argument("--idl", type=str, required=True,
+                        help="Path to idl file")
+    parser.add_argument("--addrs", nargs="+", type=str, required=True, default=[])  # TODO: @nimily how does this work? Can you correct the types and add a help message
+    parser.add_argument("--root-dir", type=str, required=True, default=os.getcwd(),
+                        help="Path to output")
+    parser.add_argument("--module", type=str, required=True,
+                        help="Name of the python module")
+    parser.add_argument("--skip-types", nargs="+", required=False, default=[],
+                        help="Idl types to skip emitting code for")
     parser.add_argument(
-        "--default-accounts", nargs="+", type=str, required=False, default=[]
-    )
+        "--default-accounts",
+        nargs="+",
+        type=str,
+        required=False,
+        default=[],
+        help="Dictionary of account name to public key which are used to enable default arguments. "
+             "Must include program_id")
     parser.add_argument(
         "--instruction-tag",
         choices=TAG_TYPES,
         default="anchor",
         required=False,
+        help="Type of instruction tag to use. Use 'anchor' if generating from an anchor idl",
     )
     parser.add_argument(
         "--account-tag",
         choices=TAG_TYPES,
         default="anchor",
         required=False,
+        help="Type of account tag to use. Use 'anchor' if generating from an anchor idl",
     )
     args = parser.parse_args()
 
@@ -57,7 +68,6 @@ def main():
     for acct in args.default_accounts:
         name, value = acct.split("=")
         if "." in value:
-
             default_accounts[name] = default_account_gen(name, value)
         else:
             default_accounts[name] = value
