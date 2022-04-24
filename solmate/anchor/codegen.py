88e267eb0fed5e87b722605aa034377998c26070
Thomas Fischer <thomasfischer@fastmail.com>
2022-04-24 16:37:31 -0400

    add support for named enum fields

--- solmate/anchor/codegen.py
+++ solmate/anchor/codegen.py
@@ -278,9 +278,23 @@ class CodeGen:
                     elif variant.fields.is_a(EnumFields.NAMED):
                         editor.add_from_import("pod", "Variant")
                         editor.add_from_import("pod", "named_fields")
-                        # TODO complete me
-                        named_fields = variant.fields.field
-                        raise NotImplementedError()
+                        editor.add_from_import("typing", "Optional")
+
+                        named_fields = []
+                        for field in variant.fields.field:
+                            field_name = field.name
+                            field_type = self._get_type_as_string(
+                                field.type,
+                                editor,
+                                within_types=True,
+                                explicit_forward_ref=True,
+                            )
+                            named_fields.append(f"{field_name}={field_type}")
+
+                        if len(named_fields) == 0:
+                            variant_type = "None"
+                        else:
+                            variant_type = "Variant(field=Optional[named_fields(" + ", ".join(named_fields) + ")])"
                     else:
                         editor.add_from_import("podite", "Variant")
 
