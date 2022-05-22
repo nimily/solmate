05bb2b734fc39ae539abc29a29004e4eabc790b3
Nima Hamidi <hamidi@alumni.stanford.edu>
2022-05-22 12:32:35 -0500

    Add a list of most commonly used mints

--- solmate/programs/token_program/__init__.py
+++ solmate/programs/token_program/__init__.py
@@ -4,7 +4,29 @@ import solmate.programs.token_program.constants as constants
 import solmate.programs.token_program.instructions as instructions
 import solmate.programs.token_program.types as types
 
-from solmate.programs.token_program.addrs import PROGRAM_ID
+from solmate.programs.token_program.addrs import (
+    ALEPH_SOLLET_MINT,
+    BNB_WORMHOLE_MINT,
+    BTC_WORMHOLE_MINT,
+    ETH_SOLLET_MINT,
+    ETH_WORMHOLE_MINT,
+    HXRO_SOLLET_MINT,
+    JSOL_MINT,
+    LUNA_WORMHOLE_MINT,
+    MNDE_MINT,
+    MNGO_MINT,
+    MSOL_MINT,
+    PROGRAM_ID,
+    RAY_MINT,
+    REN_BTC_MINT,
+    REN_DOGE_MINT,
+    SLND_MINT,
+    SOL_MINT,
+    SRM_MINT,
+    ST_SOL_MINT,
+    USDC_MINT,
+    USDT_MINT,
+)
 from solmate.programs.token_program.errors import Error
 
 # LOCK-END
